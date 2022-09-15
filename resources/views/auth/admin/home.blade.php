9a75b16e523d217806549735b6133e734b046dba
Frances Naomi Alido <55601810+namiSunflower@users.noreply.github.com>
2022-09-15 14:12:57 +0800

    edited homepage for admin

--- resources/views/auth/admin/home.blade.php
+++ resources/views/auth/admin/home.blade.php
@@ -5,20 +5,6 @@
     {{-- if(@auth) --}}
     <h1 class="text-center">Welcome {{$name}}!</h1>
     <br>
-    <h2 class="text-center">Task List:</h2>
-        @if(count($tasks) > 0)
-            @foreach($tasks as $task)
-        <div class="task-container border border-secondary text-center">
-            <h3>{{$task-> taskTitle}}</h3>
-            <small>Created on {{$task -> created_at}}</small><br>
-            <small>Updated at {{$task -> updated_at}}</small><br>
-            <a href="{{ route('tasks.show', $task->id)}}" class="btn btn-secondary">View</a>
-        </div>
-            @endforeach
-        @else
-        <p>You have no tasks. Make sure to create a new one to keep track of your goals!</p>
-        @endif  
-    <br>
-    <a href="/tasks/create" class= "btn btn-primary">Create New Task</a>
+    <h2 class="text-center">You are logged in</h2>
 </div>      
 @endsection
