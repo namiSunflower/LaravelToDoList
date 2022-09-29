2014f065bf3bbf1cfb6fffd287e0888cd21d36f5
Frances Naomi Alido <55601810+namiSunflower@users.noreply.github.com>
2022-09-29 10:26:15 +0800

    update

--- resources/views/admin/api/index.blade.php
+++ resources/views/admin/api/index.blade.php
@@ -1,29 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="text-center">
-<div class="container" id="container">
-        {{-- @if(count($user) > 0)
-            @foreach($tasks as $task)
-        <div class="task-container p-3 border border-success text-center">
-            <h3>{{$task-> taskTitle}}</h3>
-            <a href="{{ route('tasks.show', $task)}}" class="btn btn-secondary mt-3">View</a>
-        </div>
-            @endforeach
-        @else
-        <p>You have no tasks. Make sure to create a new one to keep track of your goals!</p>
-        @endif   --}}
-    <br>
-    {{-- <a href="{{ route('tasks.create')}}" class= "btn btn-primary">Create New Task</a> --}}
-    {{-- <div> --}}
-    {{-- </div> --}}
-</div>  
+<div class="text-center" id="parent">
+    <div class="container" id="container">
+    </div>  
+</div>
+<div id="button-wrapper" class="text-center">
     <button id="prev" class="btn btn-primary ml-5 mt-3 mb-3">Prev</button>
     <button id="next" class="btn btn-primary ml-5 mt-3 mb-3">Next</button>
 </div>
 @endsection
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script>
+    
     $(document).ready(function(){
         $.ajaxSetup({
                 headers: {
@@ -32,31 +21,48 @@
         });
         let current_page = 1;
         let max_pages = 1;
+        // $("#button-wrapper").remove();
         getData();
         function getData(){
-            // $("#container").remove(); 
-            // const div = document.createElement("div");
-            // div.setAttribute("id", "container");
+            const usersDiv = document.createElement("div");
+            // const buttonDiv = document.createElement("div");
+            usersDiv.setAttribute("id", "container");
+            // buttonDiv.setAttribute("id", "button-wrapper");
+            const parent = document.getElementById('parent');
+            const firstPosition = parent.firstElementChild;
+
+            // parent.insertBefore(usersDiv, parent.firstChild);
+            // document.getElementById('parent').appendChild(usersDiv);
             $('#prev').attr('disabled','disabled')
             $.ajax({
                 type:"GET",
                 url: `/api?page=${current_page}`,
                 dataType: "JSON",
                 success: function(response){
-                    $('#container').html("");
+                    $("#container").remove();
+                    const usersDiv = document.createElement("div");
+                    usersDiv.setAttribute("id", "container");
+                    document.getElementById('parent').appendChild(usersDiv);
                     $('#container').append('<h1 class="text-center">All Users:</h2>');
                     if(response.data.length != 0){
-                    $.each(response.data, function(key, user){
-                        $('#container').append(
-                            '<div class="p-3 border border-success text-center"><h3>'+user.name+'</h3>\
-                                <a href="/admin/api/'+user.id+'" class="btn btn-secondary mt-3">View</a></div>'
-                        )}
-                    )
-                    if(response.meta.links.length >= 1){
-                        current_page = response.meta.current_page;
-                        max_pages = response.meta.links.length - 2;
+                        $.each(response.data, function(key, user){
+                            $('#container').append(
+                                '<div class="p-3 border border-success text-center"><h3>'+user.name+'</h3>\
+                                    <a href="/admin/api/'+user.id+'" class="btn btn-secondary mt-3">View</a></div>'
+                            )}
+                        )
+                        if(response.meta.links.length >= 1){
+                            current_page = response.meta.current_page;
+                            max_pages = response.meta.links.length - 2;
+                        }
                     }
-                    };
+                    else{
+                        $("#prev").attr("disabled", true);
+                        $("#next").attr("disabled", true);
+                        $('#container').append(
+                            '<p>No content to show</p>'
+                        )
+                    }
                 }
                 })}
 
